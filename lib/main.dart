f9bcef1ded0bad32bac7980ae7c92c7a2feca796
zeepak <zeepak238@gmail.com>
2023-11-15 15:27:05 +0500

    update voice dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
+import 'package:device_preview/device_preview.dart';
 import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:typed_data';
@@ -10,16 +12,24 @@ import 'package:path_provider/path_provider.dart';
 
 
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+    DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+  );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Text to Speech App',
-      home: TextToSpeechApp(),
+      home: const TextToSpeechApp(),
     );
   }
 }
@@ -94,106 +104,154 @@ class _TextToSpeechAppState extends State<TextToSpeechApp> {
   ];
   
 final List<Map<String, String>> supportedvoice = [
-  
+    {'code': 'Heading', 'name': 'Arabic Egypt'},
     {'code': 'Oda', 'name': 'Oda(Female)'},
+    {'code': 'Heading', 'name': 'Arabic Saudi Arabia'},
     {'code': 'Salim', 'name': 'Salim(Male)'},
+    {'code': 'Heading', 'name': 'Bulgarian'},
     {'code': 'Dimo', 'name': 'Dimo(Male)'},
+    {'code': 'Heading', 'name': 'Catalan'},
     {'code': 'Rut', 'name': 'Rut(Female)'},
+    {'code': 'Heading', 'name': 'Chinese China'},
     {'code': 'Luli', 'name': 'Luli(Female)'},
     {'code': 'Shu', 'name': 'Shu(Female)'},
     {'code': 'Chow', 'name': 'Chow(Female)'},
     {'code': 'Wang', 'name': 'Wang(Male)'},
+    {'code': 'Heading', 'name': 'Chinese Hong Kong'},
     {'code': 'Jia', 'name': 'Jia(Female)'},
     {'code': 'Xia', 'name': 'Xia(Female)'},
     {'code': 'Chen', 'name': 'Chen(Male)'},
+    {'code': 'Heading', 'name': 'Chinese Taiwan'},
     {'code': 'Akemi', 'name': 'Akemi(Female)'},
     {'code': 'Lin', 'name': 'Lin(Female)'},
     {'code': 'Lee', 'name': 'Lee(Male)'},
+    {'code': 'Heading', 'name': 'Croatian'},
     {'code': 'Nikola', 'name': 'Nikola(Male)'},
+    {'code': 'Heading', 'name': 'Czech'},
     {'code': 'Josef', 'name': 'Josef(Male)'},
+    {'code': 'Heading', 'name': 'Danish'},
     {'code': 'Freja', 'name': 'Freja(Female)'},
+    {'code': 'Heading', 'name': 'Dutch Belgium'},
     {'code': 'Daan', 'name': 'Daan(Male)'},
+    {'code': 'Heading', 'name': 'Dutch Netherlands'},
     {'code': 'Lotte', 'name': 'Lotte(Female)'},
     {'code': 'Bram', 'name': 'Bram(Male)'},
+    {'code': 'Heading', 'name': 'English Australia'},
     {'code': 'Zoe', 'name': 'Zoe(Female)'},
     {'code': 'Isla', 'name': 'Isla(Female)'},
     {'code': 'Evie', 'name': 'Evie(Female)'},
     {'code': 'Jack', 'name': 'Jack(Male)'},
+    {'code': 'Heading', 'name': 'English Canada'},
     {'code': 'Rose', 'name': 'Rose(Female)'},
     {'code': 'Clara', 'name': 'Clara(Female)'},
     {'code': 'Emma', 'name': 'Emma(Female)'},
     {'code': 'Mason', 'name': 'Mason(Male)'},
+    {'code': 'Heading', 'name': 'English Great Britain'},
     {'code': 'Alice', 'name': 'Alice(Female)'},
     {'code': 'Nancy', 'name': 'Nancy(Female)'},
     {'code': 'Lily', 'name': 'Lily(Female)'},
     {'code': 'Harry', 'name': 'Harry(Male)'},
+    {'code': 'Heading', 'name': 'English India'},
     {'code': 'Eka', 'name': 'Eka(Female)'},
     {'code': 'Jai', 'name': 'Jai(Female)'},
     {'code': 'Ajit', 'name': 'Ajit(Male)'},
+    {'code': 'Heading', 'name': 'English Ireland'},
     {'code': 'Oran', 'name': 'Oran(Male)'},
+    {'code': 'Heading', 'name': 'English United States'},
     {'code': 'Linda', 'name': 'Linda(Female)'},
     {'code': 'Amy', 'name': 'Amy(Female)'},
     {'code': 'Mary', 'name': 'Mary(Female)'},
     {'code': 'John', 'name': 'John(Male)'},
     {'code': 'Mike', 'name': 'Mike(Male)'},
+    {'code': 'Heading', 'name': 'Finnish'},
     {'code': 'Aada', 'name': 'Aada(Female)'},
+    {'code': 'Heading', 'name': 'French Canada'},
     {'code': 'Emile', 'name': 'Emile(Female)'},
     {'code': 'Olivia', 'name': 'Olivia(Female)'},
     {'code': 'Logan', 'name': 'Logan(Female)'},
     {'code': 'Felix', 'name': 'Felix(Male)'},
+    {'code': 'Heading', 'name': 'French France'},
     {'code': 'Bette', 'name': 'Bette(Female)'},
     {'code': 'Iva', 'name': 'Iva(Female)'},
     {'code': 'Zola', 'name': 'Zola(Female)'},
     {'code': 'Axel', 'name': 'Axel(Male)'},
+    {'code': 'Heading', 'name': 'French Switzerland'},
     {'code': 'Theo', 'name': 'Theo(Male)'},
+    {'code': 'Heading', 'name': 'German Austria'},
     {'code': 'Lukas', 'name': 'Lukas(Male)'},
+    {'code': 'Heading', 'name': 'German Germany'},
     {'code': 'Hanna', 'name': 'Hanna(Female)'},
     {'code': 'Lina', 'name': 'Lina(Female)'},
     {'code': 'Jonas', 'name': 'Jonas(Male)'},
+    {'code': 'Heading', 'name': 'German Switzerland'},
     {'code': 'Tim', 'name': 'Tim(Male)'},
+    {'code': 'Heading', 'name': 'Greek'},
     {'code': 'Neo', 'name': 'Neo(Male)'},
+    {'code': 'Heading', 'name': 'Hebrew'},
     {'code': 'Rami', 'name': 'Rami(Male)'},
+    {'code': 'Heading', 'name': 'Hindi'},
     {'code': 'Puja', 'name': 'Puja(Female)'},
     {'code': 'Kabir', 'name': 'Kabir(Male)'},
+    {'code': 'Heading', 'name': 'Hungarian'},
     {'code': 'Mate', 'name': 'Mate(Male)'},
+    {'code': 'Heading', 'name': 'Indonesian'},
     {'code': 'Intan', 'name': 'Intan(Male)'},
+    {'code': 'Heading', 'name': 'Italian'},
     {'code': 'Bria', 'name': 'Bria(Female)'},
     {'code': 'Mia', 'name': 'Mia(Female)'},
     {'code': 'Pietro', 'name': 'Pietro(Male)'},
+    {'code': 'Heading', 'name': 'Japanese'},
     {'code': 'Hina', 'name': 'Hina(Female)'},
     {'code': 'Airi', 'name': 'Airi(Female)'},
     {'code': 'Fumi', 'name': 'Fumi(Female)'},
     {'code': 'Akira', 'name': 'Akira(Male)'},
+    {'code': 'Heading', 'name': 'Korean'},
     {'code': 'Nari', 'name': 'Nari(Female)'},
+    {'code': 'Heading', 'name': 'Malay'},
     {'code': 'Aqil', 'name': 'Aqil(Male)'},
+    {'code': 'Heading', 'name': 'Norwegian'},
     {'code': 'Marte', 'name': 'Marte(Female)'},
     {'code': 'Erik', 'name': 'Erik(Male)'},
+    {'code': 'Heading', 'name': 'Polish'},
     {'code': 'Julia', 'name': 'Julia(Female)'},
     {'code': 'Jan', 'name': 'Jan(Male)'},
+    {'code': 'Heading', 'name': 'Portuguese Brazil'},
     {'code': 'Marcia', 'name': 'Marcia(Female)'},
     {'code': 'Ligia', 'name': 'Ligia(Female)'},
     {'code': 'Yara', 'name': 'Yara(Female)'},
     {'code': 'Dinis', 'name': 'Dinis(Male)'},
+    {'code': 'Heading', 'name': 'Portuguese Portugal'},
     {'code': 'Leonor', 'name': 'Leonor(Female)'},
+    {'code': 'Heading', 'name': 'Romanian'},
     {'code': 'Doru', 'name': 'Doru(Male)'},
+    {'code': 'Heading', 'name': 'Russian'},
     {'code': 'Olga', 'name': 'Olga(Female)'},
     {'code': 'Marina', 'name': 'Marina(Female)'},
     {'code': 'Peter', 'name': 'Peter(Male)'},
+    {'code': 'Heading', 'name': 'Slovak'},
     {'code': 'Beda', 'name': 'Beda(Male)'},
+    {'code': 'Heading', 'name': 'Slovenian'},
     {'code': 'Vid', 'name': 'Vid(Male)'},
+    {'code': 'Heading', 'name': 'Spanish Mexico'},
     {'code': 'Juana', 'name': 'Juana(Female)'},
     {'code': 'Silvia', 'name': 'Silvia(Female)'},
     {'code': 'Teresa', 'name': 'Teresa(Female)'},
     {'code': 'Jose', 'name': 'Jose(Male)'},
+    {'code': 'Heading', 'name': 'Spanish Spain'},
     {'code': 'Camila', 'name': 'Camila(Female)'},
     {'code': 'Sofia', 'name': 'Sofia(Female)'},
     {'code': 'Luna', 'name': 'Luna(Female)'},
     {'code': 'Diego', 'name': 'Diego(Male)'},
+    {'code': 'Heading', 'name': 'Swedish'},
     {'code': 'Molly', 'name': 'Molly(Female)'},
     {'code': 'Hugo', 'name': 'Hugo(Male)'},
+    {'code': 'Heading', 'name': 'Tamil'},
     {'code': 'Sai', 'name': 'Sai(Male)'},
+    {'code': 'Heading', 'name': 'Thai'},
     {'code': 'Ukrit', 'name': 'Ukrit(Male)'},
+    {'code': 'Heading', 'name': 'Turkish'},
     {'code': 'Omer', 'name': 'Omer(Male)'},
+    {'code': 'Heading', 'name': 'Vietnamese'},
     {'code': 'Chi', 'name': 'Chi(Male)'}
 
 
@@ -310,9 +368,18 @@ void audioplayer() async {
                 });
               },
               items: supportedvoice.map<DropdownMenuItem<String>>((Map<String, String> voice) {
+                // Check if the name ends with ')'
+                bool isNormalFontWeight = voice['name']!.endsWith(')');
                 return DropdownMenuItem<String>(
+                  
                   value: voice['code'],
-                  child: Text(voice['name']!),
+                     // Make items with bold font weight unselectable
+                 enabled: isNormalFontWeight,
+                  child: Text(
+                    voice['name']!,
+                    // Apply font weight conditionally
+                   style: TextStyle(fontWeight: isNormalFontWeight ? FontWeight.normal : FontWeight.bold),
+                    ),
                 );
               }).toList(),
             ),
@@ -338,7 +405,7 @@ void audioplayer() async {
             ),
             const SizedBox(height: 16.0),
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ElevatedButton(
                   onPressed: () async {
@@ -358,14 +425,20 @@ void audioplayer() async {
                   child: const Text('Convert to Speech'),
                 ),
               
-                ElevatedButton(
-                  onPressed: downloadAudio,
-                  child: const Text('Download Audio'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    onPressed: downloadAudio,
+                    child: const Text('Download Audio'),
+                  ),
                 ),
-                isAudioAvailable ? ElevatedButton(
-                  onPressed: audioplayer, 
-                  child: const Text('Play Audio'),
-                  ) : const SizedBox(),
+                isAudioAvailable ? Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    onPressed: audioplayer, 
+                    child: const Text('Play Audio'),
+                    ),
+                ) : const SizedBox(),
 
               ],
             ),
